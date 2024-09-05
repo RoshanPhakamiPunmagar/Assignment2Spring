5030acd08c6ef78904d868bb8a6130cca5c4fd93
Caleb <caleb.davidson@cqumail.com>
2024-09-05 16:53:23 +1000

    Setup recommendation service
    Setup template for recommendation service.

---
+++ src/main/java/org/example/assignment2spring/RecomendationService.java
@@ -0,0 +1,36 @@
+
+package org.example.assignment2spring;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+interface RecommendationServiceInt {
+
+ 
+
+    // Block a user by ID
+    Recomendation getRecomendation(Customer cust);
+
+  
+   
+}
+
+@Service
+public class RecommendationService implements RecommendationServiceInt 
+{
+
+    @Override
+    public Recomendation getRecommendation(Customer cust) {
+        // access customer databaase use information such as prefered genre then randomly select a moview from the list of movies with the matchig genre
+        Recommendation recommendation = new Recommendation();
+        recommendation.setCustomer(cust);
+        Movies movie = new Movies();      
+        movie.setTitle("Test movie");
+        movie.setUrl("www.youtube.com");
+        recommendation.setMovie(movie);
+        
+        return recommendation;
+    }
+    
+}
