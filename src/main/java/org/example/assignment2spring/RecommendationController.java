5164c9a458b61f3cde49416d3dd2439155a4b0ee
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-05 23:57:16 +1000

    Update RecommendationController.java
    Added @service tag

--- src/main/java/org/example/assignment2spring/RecommendationController.java
+++ src/main/java/org/example/assignment2spring/RecommendationController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author caleb
  */
-@RestController @Data
+@RestController @Data @Service
 @RequestMapping
 public class RecommendationController {
     @Autowired
