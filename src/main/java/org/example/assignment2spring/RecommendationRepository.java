9ab2f1747923fd5298a8edf9888e63c54effe08b
chromatic-wolf <caleb.davidson@cqumail.com>
2024-09-06 17:45:01 +1000

    Format code

--- src/main/java/org/example/assignment2spring/RecommendationRepository.java
+++ src/main/java/org/example/assignment2spring/RecommendationRepository.java
@@ -8,11 +8,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-/**
- *
- * @author bruce
- */
 @RepositoryRestResource
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+
     List<Recommendation> findAll();
 }
--- src/main/java/org/example/assignment2spring/RecommendationService.java
+++ src/main/java/org/example/assignment2spring/RecommendationService.java
@@ -1,4 +1,3 @@
-
 package org.example.assignment2spring;
 
 import java.util.ArrayList;
@@ -8,31 +7,28 @@ import org.springframework.stereotype.Service;
 
 interface RecommendationServiceInt {
 
- 
-
-    // Block a user by ID
     Recommendation getRecommendation(Customer cust);
+
     List<Recommendation> getAllRecommendations();
-  
-   
+
 }
 
 @Service
-public class RecommendationService implements RecommendationServiceInt 
-{
-@Autowired
-private RecommendationRepository recommendationRepository;
+public class RecommendationService implements RecommendationServiceInt {
+
+    @Autowired
+    private RecommendationRepository recommendationRepository;
 
     @Override
     public Recommendation getRecommendation(Customer cust) {
         // access customer databaase use information such as prefered genre then randomly select a moview from the list of movies with the matchig genre
         Recommendation recommendation = new Recommendation();
         recommendation.setCustomer(cust);
-        Movies movie = new Movies();      
+        Movies movie = new Movies();
         movie.setTitle("Test movie");
         movie.setUrl("www.youtube.com");
         recommendation.setMovie(movie);
-        
+
         return recommendation;
     }
 
@@ -40,5 +36,5 @@ private RecommendationRepository recommendationRepository;
     public List<Recommendation> getAllRecommendations() {
         return recommendationRepository.findAll();
     }
-    
+
 }
