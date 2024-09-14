aaf139bcf29fec92f17a51c6cace239ed4df1d37
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-14 19:30:56 +1000

    Moved repositories controllers ect

---
+++ Database/src/main/java/WatchList/WatchListRepository.java
@@ -0,0 +1,14 @@
+package WatchList;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface WatchListRepository extends JpaRepository<WatchList, Long> {
+
+}
