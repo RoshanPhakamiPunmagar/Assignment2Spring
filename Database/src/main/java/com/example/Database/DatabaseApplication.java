14316612f7783cb2e07403e134b619d4efecc291
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-14 19:15:34 +1000

    Update DatabaseApplication.java

--- Database/src/main/java/com/example/Database/DatabaseApplication.java
+++ Database/src/main/java/com/example/Database/DatabaseApplication.java
@@ -2,12 +2,17 @@ package com.example.Database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DatabaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplicationBuilder DatabaseService = new SpringApplicationBuilder(DatabaseApplication.class);
+        DatabaseService.properties("server.port=8001");
+        // #comment in application.properties
+        DatabaseService.properties("spring.application.name=Database");
+        DatabaseService.run(args);
+    }
 
 }
