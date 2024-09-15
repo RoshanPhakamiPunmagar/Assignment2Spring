03358ad50beb9880008537f681b724c9cc40e46e
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-15 15:29:04 +1000

    What is going on

--- Database/src/main/java/com/example/Database/DatabaseApplication.java
+++ Database/src/main/java/com/example/Database/DatabaseApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
 
