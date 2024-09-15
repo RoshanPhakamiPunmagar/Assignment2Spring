6aecebd5b79d94d2817a158b737332672a2b6afb
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-15 15:43:13 +1000

    Update Recommendation.java

--- Database/src/main/java/com/example/Database/Recommendation.java
+++ Database/src/main/java/com/example/Database/Recommendation.java
@@ -4,10 +4,41 @@
  */
 package com.example.Database;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author caleb
  */
+@Entity
+@Data
+@NoArgsConstructor
+
 public class Recommendation {
-    
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToOne
+    private Movies movie;
+    @ManyToOne
+    private Customer customer;
+
+    public void addMovie(Movies movie) {
+        if (!this.movie.equals(movie)) {
+            this.movie = movie;
+        }
+    }
+
+    public void addCustomer(Customer cust) {
+        if (!this.customer.equals(cust)) {
+            this.customer = cust;
+        }
+    }
 }
