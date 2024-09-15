18b9e1217114586a0867842e6e9b3d71f1af22e7
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-15 22:21:09 +1000

    Added update user path

--- Database/src/main/java/com/example/Database/CustomerController.java
+++ Database/src/main/java/com/example/Database/CustomerController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,26 +29,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Service
 @RequestMapping
 public class CustomerController {
-    
+
     @Autowired
     private CustomerRepository customerRepository;
 
-    
-     @GetMapping("/getCustomer/{custId}")
-    public  ResponseEntity<Customer> getUser(@PathVariable long custId) {
-         Customer cust =  customerRepository.findById(custId).get();
-         return ResponseEntity.ok(cust);
+    @GetMapping("/getCustomer/{custID}")
+    public ResponseEntity<Customer> getUser(@PathVariable long custID) {
+        Customer cust = customerRepository.findById(custID).get();
+        return ResponseEntity.ok(cust);
     }
+
     @GetMapping("/getCustomers/")
-    public  ResponseEntity<List<Customer>> getUser() {
-         List<Customer> cust =  customerRepository.findAll();
-         return ResponseEntity.ok(cust);
+    public ResponseEntity<List<Customer>> getUser() {
+        List<Customer> cust = customerRepository.findAll();
+        return ResponseEntity.ok(cust);
     }
-    @DeleteMapping("/deleteUser/{custId}")
-    public void deleteUser(@PathVariable long custId)
-    {
-        customerRepository.deleteById(custId);
+
+    @DeleteMapping("/deleteCustomer/{custID}")
+    public void deleteUser(@PathVariable long custID) {
+        customerRepository.deleteById(custID);
     }
-    
+
+    @PutMapping("/updateCustomer/{custID")
+    public void updateUser(@PathVariable long custID, @RequestBody Customer cust) {
+
+        Customer user = customerRepository.findById(custID).get();
+        user = cust;
+        customerRepository.save(user);
+
+    }
 
 }
