9a199b8b471b54e6e061ab3af230858d5a7065a5
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:44:53 +1000

    Added data classes

---
+++ WebApp/src/main/java/com/example/WebApp/Customer.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.WebApp;
+
+/**
+ *
+ * @author caleb
+ */
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    private boolean blocked = false;  // Renamed field to 'blocked'
+
+    @ManyToMany
+    private List<Movie> movies = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+
+        this.movies.add(movie);
+        if (!this.movies.contains(movie)) {
+            movie.addCustomer(this);
+
+        }
+    }
+
+    public void blockUser() {
+        this.blocked = true;
+    }
+
+    public void unblockUser() {
+        this.blocked = false;
+    }
+}
---
+++ WebApp/src/main/java/com/example/WebApp/WatchList.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.WebApp;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class WatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NonNull
+
+    private long movieId;
+    @ManyToMany(mappedBy = "watchLists")
+    private List<Movie> moveList = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+        if (!this.moveList.contains(movie)) {
+
+            this.moveList.add(movie);
+            System.out.println("Movie added: " + movie.getId() + " " + this.id);
+        }
+
+    }
+
+    public Long getWatchListId() {
+        return this.id;
+    }
+
+    public void removeMovie(Movie movie) {
+        if (!this.moveList.contains(movie)) {
+            for (Movie m : this.moveList) {
+                if (movie.getId().equals(m.getId())) {
+                    this.moveList.remove(movie);
+                }
+
+            }
+            System.out.println("Movie removed: " + movie.getId() + " " + this.id);
+        }
+
+    }
+
+}
