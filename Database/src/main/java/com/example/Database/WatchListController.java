23e0da0343c9c555d530e1b7320d90dca5841e3d
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 22:52:15 +1000

    Add annotations

--- Database/src/main/java/com/example/Database/WatchListController.java
+++ Database/src/main/java/com/example/Database/WatchListController.java
@@ -4,10 +4,19 @@
  */
 package com.example.Database;
 
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author caleb
  */
+@RestController
+@Data
+@Service
+@RequestMapping
 public class WatchListController {
     
 }
