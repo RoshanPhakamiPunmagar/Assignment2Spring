2b42892cb2f8ec26939901e50635c686c0a1c39c
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:55:20 +1000

    added webAppClient

--- WebApp/src/main/java/com/example/WebApp/WebAppController.java
+++ WebApp/src/main/java/com/example/WebApp/WebAppController.java
@@ -5,6 +5,7 @@
 package com.example.WebApp;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Data
 @RequestMapping
 public class WebAppController {
+
+    @Autowired
+    private WebAppClient webAppClient;
 
     @GetMapping("/movieRecommendation")
     public ModelAndView recommendMovie() {
