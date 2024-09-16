4c1b91ec152a83f7f609af1c56a120191c604a85
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 22:15:59 +1000

    Added screamer service

---
+++ ScreamerService/src/main/java/com/example/ScreamerService/ScreamerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScreamerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreamerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreamerServiceApplication.class, args);
+	}
+
+}
---
+++ ScreamerService/src/test/java/com/example/ScreamerService/ScreamerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScreamerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreamerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
