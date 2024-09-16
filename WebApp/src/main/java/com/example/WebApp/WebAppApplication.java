eb1a15b1870fed35262d64be3bdb5ec909aef72c
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:43:39 +1000

    Added microservice code

--- WebApp/src/main/java/com/example/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/com/example/WebApp/WebAppApplication.java
@@ -2,12 +2,22 @@ package com.example.WebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class WebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplicationBuilder webApp = new SpringApplicationBuilder(WebAppApplication.class);
+        webApp.properties("server.port=8080");
+        webApp.properties("spring.application.name=recommendationClient");
+        webApp.properties("eureka.client.service-url.defaultZone=http://localhost:8761/eureka/");
+        webApp.properties("eureka.instance.prefer-ip-address=true");
+        webApp.run(args);
+    }
 
 }
