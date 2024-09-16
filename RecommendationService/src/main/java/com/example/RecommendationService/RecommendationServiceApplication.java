df1f3af35a890c0e9bf220647ac95ba79f81d010
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:52:59 +1000

    Update service name

--- RecommendationService/src/main/java/com/example/RecommendationService/RecommendationServiceApplication.java
+++ RecommendationService/src/main/java/com/example/RecommendationService/RecommendationServiceApplication.java
@@ -14,7 +14,7 @@ public class RecommendationServiceApplication {
     public static void main(String[] args) {
         SpringApplicationBuilder recommendationService = new SpringApplicationBuilder(RecommendationServiceApplication.class);
         recommendationService.properties("server.port=8333");
-        recommendationService.properties("spring.application.name=recommendationClient");
+        recommendationService.properties("spring.application.name=recommendationService");
         recommendationService.properties("eureka.client.service-url.defaultZone=http://localhost:8761/eureka/");
         recommendationService.properties("eureka.instance.prefer-ip-address=true");
         recommendationService.run(args);
