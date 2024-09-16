596846818e78500b6213d880342a468a0da293e8
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:25:19 +1000

    Added annotations

--- Database/src/main/java/com/example/Database/MoviesController.java
+++ Database/src/main/java/com/example/Database/MoviesController.java
@@ -5,15 +5,23 @@
 package com.example.Database;
 
 import java.util.List;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author caleb
  */
+@RestController
+@Data
+@Service
+@RequestMapping
 public class MoviesController {
 
     @Autowired
