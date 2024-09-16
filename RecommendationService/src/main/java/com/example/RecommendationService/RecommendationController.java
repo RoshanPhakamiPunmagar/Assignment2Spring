a38c33b8b98f950298d6e4352eafc456e49a740f
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 18:38:01 +1000

    Changed to return object

--- RecommendationService/src/main/java/com/example/RecommendationService/RecommendationController.java
+++ RecommendationService/src/main/java/com/example/RecommendationService/RecommendationController.java
@@ -10,6 +10,7 @@ import java.util.Random;
 import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,9 +27,9 @@ public class RecommendationController {
     private final RecommendationClient recommendationClient; //this will include the @FeignClient intefrace above, then you can call the retrieve function which is mapped to /dogs/{breed}. The feign client then connects to back end service retrieves data and passes to webapp
     Random random = new Random();
 
-    @GetMapping("/ui/recommendation/{custID}")
+    @GetMapping("/recommendation/{custID}")
     @ResponseBody // can return webpage in stead
-    String _retrieve(@PathVariable Long custID) {
+    ResponseEntity<Recommendation> getRecommendation(@PathVariable Long custID) {
 
         Customer cust = recommendationClient.getCustomer(custID);
         Recommendation recommendation = new Recommendation();
@@ -36,7 +37,7 @@ public class RecommendationController {
         //get random movie and add to recommendation then save recommendation
         recommendation.setMovieUrl(getRandomMovie().getUrl());
 
-        return "<h1>" + recommendation + "</h1>" + '\n' + "<h1>" + cust + "<h1>";
+        return ResponseEntity.ok(recommendation);
 
     }
 
