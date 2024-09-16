78f77d1cf19373d123262bd5dbf353f9f70303a9
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-16 22:20:26 +1000

    Added getmovies openfeign

--- ScreamerService/src/main/java/com/example/ScreamerService/screamerClient.java
+++ ScreamerService/src/main/java/com/example/ScreamerService/screamerClient.java
@@ -4,10 +4,19 @@
  */
 package com.example.ScreamerService;
 
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 /**
  *
  * @author caleb
  */
-public class screamerClient {
-    
+@FeignClient(name = "Database", url = "http://localhost:8000")
+interface ScreamerClient {
+
+    @GetMapping("/getMovies")
+    List<Movie> getMovies();
+
 }
