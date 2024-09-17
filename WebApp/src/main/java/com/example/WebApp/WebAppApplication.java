a052c640d374a188e032b5c093f072ef4c76e8b1
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-17 23:24:15 +1000

    Added basic auth

--- WebApp/src/main/java/com/example/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/com/example/WebApp/WebAppApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -20,4 +30,34 @@ public class WebAppApplication {
         webApp.run(args);
     }
 
+}
+
+@Configuration
+class _SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager _userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("password").roles("USER").build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("password").roles("ADMIN").roles("USER").build();
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(user);
+        manager.createUser(admin);
+        return manager;
+    }
+
+    @Bean
+    SecurityFilterChain _filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request
+                        -> request.requestMatchers(HttpMethod.GET, "/user/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/admin/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
 }
