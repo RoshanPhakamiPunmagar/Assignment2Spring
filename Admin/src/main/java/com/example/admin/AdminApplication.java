3f16987a00b43e2054f3f8a4f3814b0c2ab7733f
Roshan <r.phakamipunmagar@cqumail.com>
2024-09-19 20:45:54 +1000

    comments

--- Admin/src/main/java/com/example/admin/AdminApplication.java
+++ Admin/src/main/java/com/example/admin/AdminApplication.java
@@ -6,11 +6,26 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@SpringBootApplication @EnableDiscoveryClient
+
+/**
+ *
+ * @author Roshan Phakami PunMagar
+ * File Name: AdminApplication.java
+ * Date :16/9/2024
+ * Purpose : Runs AdminApplicaiton
+ * ******************************************************
+ */
+//Enable Spring Boot application
+@SpringBootApplication
+// Enables service discovery for this application
+@EnableDiscoveryClient
+// Enables Feign clients for making RESTFUL HTTP requests to other services
 @EnableFeignClients
 public class AdminApplication {
 
     public static void main(String[] args) {
+
+        // Starts the Spring Boot application
         SpringApplication.run(AdminApplication.class, args);
     }
 
