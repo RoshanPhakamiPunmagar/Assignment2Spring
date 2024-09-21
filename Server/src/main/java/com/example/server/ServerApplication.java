540a58f430156972c6c498d0ed99798b08403848
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-21 21:55:04 +1000

    Update ServerApplication.java

--- Server/src/main/java/com/example/server/ServerApplication.java
+++ Server/src/main/java/com/example/server/ServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
