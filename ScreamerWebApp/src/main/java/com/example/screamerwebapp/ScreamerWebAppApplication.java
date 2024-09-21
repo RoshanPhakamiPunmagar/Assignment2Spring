8345092378f232b078db6797766390613327bfd8
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-21 21:59:15 +1000

    rename

--- ScreamerWebApp/src/main/java/com/example/screamerwebapp/ScreamerWebAppApplication.java
+++ ScreamerWebApp/src/main/java/com/example/screamerwebapp/ScreamerWebAppApplication.java
@@ -55,12 +55,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ScreamerWebAppApplication {
 
     public static void main(String[] args) {
-        SpringApplicationBuilder webApp = new SpringApplicationBuilder(ScreamerWebAppApplication.class);
-        webApp.properties("server.port=8080");
-        webApp.properties("spring.application.name=recommendationClient");
-        webApp.properties("eureka.client.service-url.defaultZone=http://localhost:8761/eureka/");
-        webApp.properties("eureka.instance.prefer-ip-address=true");
-        webApp.run(args);
+        SpringApplicationBuilder screamerWebApp = new SpringApplicationBuilder(ScreamerWebAppApplication.class);
+        screamerWebApp.properties("server.port=8080");
+        screamerWebApp.properties("spring.application.name=recommendationClient");
+        screamerWebApp.properties("eureka.client.service-url.defaultZone=http://localhost:8761/eureka/");
+        screamerWebApp.properties("eureka.instance.prefer-ip-address=true");
+        screamerWebApp.run(args);
     }
 
 }
