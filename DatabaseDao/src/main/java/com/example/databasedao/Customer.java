2df4e00c3b50ffc65187ef7713aa827f6d8725ae
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-21 22:41:02 +1000

    removed unused imports

--- DatabaseDao/src/main/java/com/example/databasedao/Customer.java
+++ DatabaseDao/src/main/java/com/example/databasedao/Customer.java
@@ -1,16 +1,12 @@
 package com.example.databasedao;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 
 @Entity
 @Data
