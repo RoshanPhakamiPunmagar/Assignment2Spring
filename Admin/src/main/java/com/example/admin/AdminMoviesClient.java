11a84b23689419364ae238aa59202d8ff0849684
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2024-09-21 22:17:07 +1000

    remove url dependency

--- Admin/src/main/java/com/example/admin/AdminMoviesClient.java
+++ Admin/src/main/java/com/example/admin/AdminMoviesClient.java
@@ -18,7 +18,7 @@ import java.util.List;
  * ******************************************************
  */
 
-@FeignClient(name = "database", url = "http://localhost:8009")
+@FeignClient(name = "database")
 // Feign client interface to communicate with the "admin-movies" microservice.
 // Base URL for the movies service is: http://localhost:8009/admin/movies
 interface AdminMoviesClient {
